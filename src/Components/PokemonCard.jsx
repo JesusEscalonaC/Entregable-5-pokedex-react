2dd10d646761f68248aa179569b808be159a3b0c
JesusEscalonaC <jesusdescalonac@gmail.com>
2022-11-22 20:59:41 -0400

    22-11-22 8:59pm

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -17,6 +17,10 @@ const PokemonCard = ({url}) => {
     const getTypes = () =>{
         return pokemon.types?.map( type => " "+type.type.name)
       }
+    const getTypesCard = () =>{
+       return pokemon.types?.[0].type.name
+    }
+    console.log(pokemon)
     return (
        
         <div className="pokemon">
@@ -33,31 +37,33 @@ const PokemonCard = ({url}) => {
             <img src={abajo} className="pokeball__img3" alt="" />
           </div>
         </Link>
-        <div class="pokeball__card" >
-        <h2 className='card-title'>{pokemon.name}</h2> 
-        <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
-        <h4 className='card-text'>
+        <div className= {`pokeball__card ${getTypesCard()}`} >
+        <img className='pokedex-img' src={pokemon.sprites?.other.dream_world.front_default} alt="" />
+        <h2 className='card-title'>{pokemon.name?.toUpperCase()}</h2> 
+        <h4 className='card-subtitle'>
           Types: 
           {
             getTypes()
           }
         </h4 >
 
+        <div className='card-text-container'>
         <h4 className='card-text'>
-          Hp: { pokemon.stats?.[0].base_stat }
+         HP: { pokemon.stats?.[0].base_stat }
         </h4 >
 
         <h4 className='card-text'>
-          Attack: { pokemon.stats?.[1].base_stat }
+          ATTACK: { pokemon.stats?.[1].base_stat}
         </h4 >
 
         <h4 className='card-text'>
-          Defense: { pokemon.stats?.[2].base_stat }
+          DEFENSE: { pokemon.stats?.[2].base_stat }
         </h4 >
 
         <h4 className='card-text'>
-          Speed: { pokemon.stats?.[5].base_stat }
+          SPEED: { pokemon.stats?.[5].base_stat }
         </h4 >
+        </div>
         </div>
       </div>
     </div>
