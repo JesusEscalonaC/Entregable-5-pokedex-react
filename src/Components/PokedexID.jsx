a993613f0593fe789c99278bb896a97cb80c7897
Oscar Tandioy <tato.tandioy@gmail.com>
2022-11-25 00:10:21 -0500

    25/11/2022 12:10 am

--- src/Components/PokedexID.jsx
+++ src/Components/PokedexID.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 
 const PokedexID = () => {
   const [pokemon, setPokemon] = useState({});
+  const [ whidthPro, setWidthPro ] = useState(0);
 
   const { id } = useParams();
   useEffect(() => {
@@ -12,69 +13,119 @@ const PokedexID = () => {
       .then(res => setPokemon(res.data))
   }, [])
 
-  const getTypes = () =>{
-    return pokemon.types?.map( type => " "+type.type.name)
+  useEffect(() => {
+    if(whidthPro <= 100){
+      setTimeout(function() {
+        setWidthPro(whidthPro+1);
+      }, 60);
+    }
+  }, [whidthPro])
+
+  const getTypes = () => {
+    return pokemon.types?.map(type => " " + type.type.name)
   }
 
   // console.log(pokemon)
-  
+
   return (
     <div className='pokedex-id'>
       <div className='pokemon-detail-container' >
         <h2>{pokemon.name}</h2>
         <div className="pokemon-detail">
 
-            
-            <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
 
-            <div className='abilites' >
-              <h3>Abilities</h3>
-              <ul>
-                {pokemon.abilities?.map(ability => (
-                  <li key={ability.ability.name} >{ability.ability.name}</li>
-                ))
-                }
-              </ul>
-            </div>
+          <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
 
-            
-            <div className='pokemon-detail-text' >
+          <div className='abilites' >
+            <h3>Abilities</h3>
+            <ul>
+              {pokemon.abilities?.map(ability => (
+                <li key={ability.ability.name} >{ability.ability.name}</li>
+              ))
+              }
+            </ul>
+          </div>
 
-              <h4>
-                Types:
-                {
-                  getTypes()
-                }
-              </h4 >
 
+          <div className='pokemon-detail-text' >
+
+            <h4>
+              Types:
+              {
+                getTypes()
+              }
+            </h4 >
+
+            <div className='pokemon-progress-container'>
               <h4>
                 Hp: {pokemon.stats?.[0].base_stat}
               </h4 >
+              <div className='bar'>
+                <div 
+                  className='progress' 
+                  style={{width:`${whidthPro < pokemon.stats?.[0].base_stat ? whidthPro : pokemon.stats?.[0].base_stat }%  `}}
+                ></div>
+              </div>
+            </div>
+            
+            <div className="pokemon-progress-container">
 
               <h4>
                 Attack: {pokemon.stats?.[1].base_stat}
               </h4 >
 
+              <div className='bar'>
+                <div 
+                  className='progress' 
+                  style={{width:`${whidthPro < pokemon.stats?.[1].base_stat ? whidthPro : pokemon.stats?.[1].base_stat }%  `}}
+                ></div>
+              </div>
+
+            </div>
+
+            <div className="pokemon-progress-container">
+
               <h4>
                 Defense: {pokemon.stats?.[2].base_stat}
               </h4 >
+
+              <div className='bar'>
+                <div 
+                  className='progress' 
+                  style={{width:`${whidthPro < pokemon.stats?.[2].base_stat ? whidthPro : pokemon.stats?.[2].base_stat }%  `}}
+                ></div>
+              </div>
+
+            </div>
+
+            <div className="pokemon-progress-container">
 
               <h4>
                 Speed: {pokemon.stats?.[5].base_stat}
               </h4 >
 
+              <div className='bar'>
+                <div 
+                  className='progress' 
+                  style={{width:`${whidthPro < pokemon.stats?.[5].base_stat ? whidthPro : pokemon.stats?.[5].base_stat }%  `}}
+                ></div>
+              </div>
+
             </div>
+
+
+          </div>
 
         </div>
 
       </div>
-      
+
       <div className='list-moves-container'>
         <h2>Moves</h2>
         <ul className='list-moves' >
           {pokemon.moves?.map(move => (
-              <li key={move.move.name} >{move.move.name}</li>
-            ))
+            <li key={move.move.name} >{move.move.name}</li>
+          ))
           }
         </ul>
       </div>
